c646e03cfd951ecf6b51bd20aea9ed20a42848a8
Peter Dunbar <Peter.joseph.dunbar@gmail.com>
2014-05-22 15:23:49 -0400

    We should have done more commits up until this point.
    Still need to re-render '/new' with previously entered form field values.

--- server.rb
+++ server.rb
@@ -1,1 +1,41 @@
 require 'sinatra'
+require 'csv'
+require 'shotgun'
+require 'pry'
+
+def load_list
+  articles =[]
+  article = nil
+  CSV.foreach('articles.csv', headers: true) do |row|
+    article = {
+      name: row ["name"],
+      title: row["title"],
+      url: row["url"],
+      description: row["description"]
+    }
+  articles << article
+  end
+  articles
+end
+
+
+get '/' do
+@articles = load_list
+
+erb :home
+end
+
+get '/new' do
+
+  erb :new
+end
+
+post '/new' do
+ @url = params[:url]
+
+    article = [params[:name],params[:new_article],params[:url], params[:description]]
+    CSV.open('articles.csv', 'a') do |csv|
+      csv << article
+  end
+  redirect '/'
+end
